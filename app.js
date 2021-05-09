692b352b8300190763bf05498f461cb981043f94
soojinahn <soojahnx@gmail.com>
2021-05-09 06:23:50 -0400

    Made changes to app.js organization

--- app.js
+++ app.js
@@ -18,6 +18,71 @@ const users = [
     }
 ];
 
+const getHashedPassword = (password) => {
+    const sha256 = crypto.createHash('sha256');
+    const hash = sha256.update(password).digest('base64');
+    return hash;
+}
+
+const generateAuthToken = () => {
+    return crypto.randomBytes(30).toString('hex');
+}
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+    const authToken = req.cookies['AuthToken'];
+    req.user = authTokens[authToken];
+    next();
+});
+
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+
+app.set('view engine', 'hbs');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    const hashedPassword = getHashedPassword(password);
+
+    const user = users.find(u => {
+        return u.email === email && hashedPassword === u.password
+    });
+
+    if (user) {
+        const authToken = generateAuthToken();
+
+        // Store authentication token
+        authTokens[authToken] = user;
+
+        // Setting the auth token in cookies
+        res.cookie('AuthToken', authToken);
+
+        // Redirect user to the protected page
+        res.redirect('/protected');
+    } else {
+        res.render('login', {
+            message: 'Invalid username or password',
+            messageClass: 'alert-danger'
+        });
+    }
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
 app.post('/register', (req, res) => {
     const { email, firstName, lastName, password, confirmPassword } = req.body;
 
@@ -57,79 +122,6 @@ app.post('/register', (req, res) => {
     }
 });
 
-app.post('/login', (req, res) => {
-    const { email, password } = req.body;
-    const hashedPassword = getHashedPassword(password);
-
-    const user = users.find(u => {
-        return u.email === email && hashedPassword === u.password
-    });
-
-    if (user) {
-        const authToken = generateAuthToken();
-
-        // Store authentication token
-        authTokens[authToken] = user;
-
-        // Setting the auth token in cookies
-        res.cookie('AuthToken', authToken);
-
-        // Redirect user to the protected page
-        res.redirect('/protected');
-    } else {
-        res.render('login', {
-            message: 'Invalid username or password',
-            messageClass: 'alert-danger'
-        });
-    }
-});
-
-const getHashedPassword = (password) => {
-    const sha256 = crypto.createHash('sha256');
-    const hash = sha256.update(password).digest('base64');
-    return hash;
-}
-
-const generateAuthToken = () => {
-    return crypto.randomBytes(30).toString('hex');
-}
-
-app.use((req, res, next) => {
-    // Get auth token from the cookies
-    const authToken = req.cookies['AuthToken'];
-
-    // Inject the user to the request
-    req.user = authTokens[authToken];
-
-    next();
-});
-
-// To support URL-encoded bodies
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// To parse cookies from the HTTP Request
-app.use(cookieParser());
-
-app.engine('hbs', exphbs({
-    extname: '.hbs'
-}));
-
-app.set('view engine', 'hbs');
-
-// Our requests hadlers will be implemented here...
-
-app.get('/', function (req, res) {
-    res.render('home');
-});
-
-app.get('/register', (req, res) => {
-    res.render('register');
-});
-
-app.get('/login', (req, res) => {
-    res.render('login');
-});
-
 app.get('/protected', (req, res) => {
     if (req.user) {
         res.render('protected');
@@ -140,5 +132,6 @@ app.get('/protected', (req, res) => {
         });
     }
 });
+
 
 app.listen(3000);
